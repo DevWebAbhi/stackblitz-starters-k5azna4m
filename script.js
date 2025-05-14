3d7becaa4107144ace8477ec92b8899bcf6691c9
DevWebAbhi <tiwariabhishek889912@gmail.com>
2025-05-14 10:12:01 +0530

    task-completed

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+
+const SECRET_KEY = 'hgchgv-jblonjnolbnboun';
+
+
 const encrypt = (payload) => {
-  // encrypt the payload and return token
-}
+  try {
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' }); // Token expires in 1 hour
+    return token;
+  } catch (error) {
+    console.error('Error encrypting payload:', error);
+    throw error;
+  }
+};
+
 
 const decrypt = (token) => {
-  // return decoded payload
-}
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    return decoded;
+  } catch (error) {
+    console.error('Error decrypting token:', error);
+    throw error;
+  }
+};
+const token = encrypt({test:"test"});
+console.log(token);
+const decrypted = decrypt(token)
+console.log(decrypted)
 
 module.exports = {
   encrypt,
   decrypt
-}
+};
